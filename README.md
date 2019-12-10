263a2094c0ae0fc8debcdba15281e5d7363328f6
Brian Mason <bpmason1@users.noreply.github.com>
2019-12-10 17:51:16 -0500

    command line args

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # corsair
 CORS-ignoring proxy
+
+### run the proxy
+`corsair --listen-ip=127.0.0.1:8000 --proxy-ip=127.0.0.1:8080`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello World!");
+    let matches = get_command_line_matches();
+    let listen_ip_str = matches.value_of("listen-ip").expect("listen-ip could not be read");
+    let proxy_ip_str = matches.value_of("proxy-ip").expect("proxy-ip could not be read");
+
+    println!("{}", listen_ip_str);
+    println!("{}", proxy_ip_str);
+}
+
+fn get_command_line_matches() -> clap::ArgMatches<'static> {
+    return App::new("corsair")
+        .arg(
+            Arg::with_name("listen-ip")
+                .long("listen-ip")
+                .value_name("listen-ip")
+                .takes_value(true)
+                .help(
+                    "address where the application listens for incoming messages. example: 0.0.0.0:8687",
+                ),
+        )
+        .arg(
+            Arg::with_name("proxy-ip")
+                .long("proxy-ip")
+                .value_name("proxy-ip")
+                .takes_value(true)
+                .help(
+                    "address where the application proxies incoming messages. example: 0.0.0.0:8687",
+                ),
+        )
+        .get_matches();
 }
