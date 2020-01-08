9d0b3f22ca183ade13537c5a5e3c84b29c8af969
Brian Mason <bpmason1@hotmail.com>
2020-01-07 22:37:15 -0500

    use updated fastforward API

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 CORS-ignoring proxy
 
 ### run the proxy
-`corsair --listen-ip=127.0.0.1:8000 --proxy-ip=127.0.0.1:8080`
+`corsair --listen-ip=127.0.0.1:8000 --proxy-ip=127.0.0.1:8080 --permissive`
+
+The `--permissive` flag indicates that all CORS requests should be accepted for proxying.
+The flag is required for now because I have not yet implemented logic to configure CORS rules.
+
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,64 @@
 extern crate clap;
 extern crate fastforward;
+extern crate http;
+extern crate term;
 
 use clap::{Arg, App};
-use fastforward::simple_proxy;
+use fastforward::generic_proxy;
+use http::{
+    header::HeaderValue,
+    Method,
+    Response,
+    StatusCode
+};
 use std::net::SocketAddr;
 
 
+fn my_director(req: &mut http::Request<Vec<u8>>) -> Option<Response<Vec<u8>>> { 
+    match req.method() {
+        &Method::OPTIONS => {
+            let body = Vec::new();
+            let resp = Response::builder()
+                        .status(StatusCode::NO_CONTENT)
+                        .body(body).unwrap();
+            Some(resp)
+        },
+        _ => {
+            let matches = get_command_line_matches();
+            let proxy_addr_str = matches.value_of("proxy-ip").expect("proxy-ip could not be read");
+
+            let proxy_addr = HeaderValue::from_str(proxy_addr_str).unwrap();
+
+            let req_headers = req.headers_mut();
+            req_headers.remove(http::header::HOST);
+            req_headers.insert(http::header::HOST, proxy_addr);
+
+            None
+        }
+    }
+}
+
+
 fn main() {
+    let mut terminal = term::stdout().unwrap();
+
     let matches = get_command_line_matches();
     let listen_ip_str = matches.value_of("listen-ip").expect("listen-ip could not be read");
     let listen_ip = listen_ip_str.parse::<SocketAddr>().unwrap();
 
-    let proxy_ip_str = matches.value_of("proxy-ip").expect("proxy-ip could not be read");
-    let proxy_ip = proxy_ip_str.parse::<SocketAddr>().unwrap();
+    // This is to ensure that the "proxy-ip" parameter was passed in
+    match matches.value_of("proxy-ip") {
+        Some(_) => {
+            generic_proxy(listen_ip, my_director)
+        },
+        None => {
+            terminal.fg(term::color::RED).unwrap();
+            println!("ERROR: proxy-ip could not be read");
+            terminal.reset().unwrap();
+        }
+    };
 
-    simple_proxy(listen_ip, proxy_ip)
+    
 }
 
 fn get_command_line_matches() -> clap::ArgMatches<'static> {
@@ -24,6 +68,7 @@ fn get_command_line_matches() -> clap::ArgMatches<'static> {
                 .long("listen-ip")
                 .value_name("listen-ip")
                 .takes_value(true)
+                .required(true)
                 .help(
                     "address where the application listens for incoming messages. example: 0.0.0.0:8000",
                 ),
@@ -33,8 +78,19 @@ fn get_command_line_matches() -> clap::ArgMatches<'static> {
                 .long("proxy-ip")
                 .value_name("proxy-ip")
                 .takes_value(true)
+                .required(true)
                 .help(
                     "address where the application proxies incoming messages. example: 127.0.0.1:8888",
+                ),
+        )
+        .arg(
+            Arg::with_name("permissive")
+                .long("permissive")
+                .value_name("permissive")
+                .takes_value(false)
+                .required(true)
+                .help(
+                    "if permissive is true then allow CORS for all requests/domains",
                 ),
         )
         .get_matches();
